d72aaae76927bb863b8017872687461e483d24c7
ouki <358023925@qq.com>
2018-01-17 16:07:06 +0800

    Update stepCounter.ino

--- examples/stepCounter/stepCounter.ino
+++ examples/stepCounter/stepCounter.ino
@@ -6,9 +6,10 @@
   *   0x69: set I2C address by parameter
   *
   * Through the example, you can get the sensor data which means step counter.
+  * set step counter power mode by setStepPowerMode(stepNormalPowerMode means normal model,stepLowPowerMode means low power model)
   * upload interrupt number by setInt (choose int1 or int2)  
   * data from int1 and int2 read in readStepCounter
-  * 
+  *
   * Copyright   [DFRobot](http://www.dfrobot.com), 2016
   * Copyright   GNU Lesser General Public License
   *
